74b5a131aec3aed8d10c0cb1794914bea058e8b6
syprayt <sypersyprayt@gmail.com>
2023-10-30 17:07:00 +0200

    Less_5_1

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+def first_function():
+    pass
+
+class Human:
+    pass
+
+rq = requests
+first_f = first_function
+evgen = Human
+
+print(requests.__name__)
+print(rq.__name__)
+print(first_function.__name__)
+print(first_f.__name__)
+print(Human.__name__)
+print(evgen.__name__)
+
+
+
+
+
+
+
